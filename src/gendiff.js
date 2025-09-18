1f6b0256e4d2262d6d956027705ab626acc68dbf
vlad <vladislav.nikolaenko@p2p.org>
2025-09-18 18:13:29 +0700

    Add usage and description options

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,7 +3,9 @@
 import { program } from 'commander';
 
 program
+  .usage('[options] <filepath1> <filepath2>')
   .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
   .description('Compares two configuration files and shows a difference.')
   .parse(process.argv);
 
