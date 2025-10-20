2bf7a237b8546a6a6e8cdae2ba93dd5f2c53fea5
Vladislav <nickolaenkovv@gmail.com>
2025-10-20 10:51:10 +0700

    Update diff.js

--- src/diff.js
+++ src/diff.js
@@ -7,7 +7,7 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const [content1, content2] = [readFile(filepath1), readFile(filepath2)];
   const [data1, data2] = [
     parseFileContent(filepath1, content1),
-    parseFileContent(filepath2, content2)
+    parseFileContent(filepath2, content2),
   ];
   const diffTree = buildDiffTree(data1, data2);
   return formatDiff(diffTree, formatName);
