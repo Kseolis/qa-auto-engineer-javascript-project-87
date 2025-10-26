02510386ca5090737251781aa8831878ece7f163
vlad <vladislav.nikolaenko@p2p.org>
2025-10-26 08:54:25 +0700

    refactoring: deleted extra array

--- src/diff.js
+++ src/diff.js
@@ -4,11 +4,8 @@ import { buildDiffTree } from './diff/index.js'
 import { formatDiff } from './formatters/index.js'
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const [content1, content2] = [readFile(filepath1), readFile(filepath2)]
-  const [data1, data2] = [
-    parseFileContent(filepath1, content1),
-    parseFileContent(filepath2, content2),
-  ]
+  const data1 = parseFileContent(filepath1, readFile(filepath1))
+  const data2 = parseFileContent(filepath2, readFile(filepath2))
   const diffTree = buildDiffTree(data1, data2)
   return formatDiff(diffTree, formatName)
 }
