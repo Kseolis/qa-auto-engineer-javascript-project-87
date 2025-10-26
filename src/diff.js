ccad86e96553390e2ef3d86c23debcb02a18c559
vlad <vladislav.nikolaenko@p2p.org>
2025-10-26 11:16:30 +0700

    refactoring: improve reader logic

--- src/diff.js
+++ src/diff.js
@@ -1,11 +1,10 @@
-import { parseFileContent } from './parsers.js'
 import { buildDiffTree } from './diff/index.js'
 import { formatDiff } from './formatters/index.js'
 import { readFile } from './utils.js'
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parseFileContent(filepath1, readFile(filepath1))
-  const data2 = parseFileContent(filepath2, readFile(filepath2))
+  const data1 = readFile(filepath1)
+  const data2 = readFile(filepath2)
   const diffTree = buildDiffTree(data1, data2)
   return formatDiff(diffTree, formatName)
 }
