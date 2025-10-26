d883ffc0ebf169187de180cdbd8451d53756c8bd
vlad <vladislav.nikolaenko@p2p.org>
2025-10-26 17:08:55 +0700

    refactoring: - buildDiffTree used sortBy for generate allKeys now - definitions are simplified for buildDiffTree

--- src/diff/builder.js
+++ src/diff/builder.js
@@ -1,7 +1,9 @@
+import _ from 'lodash'
+
 export const buildDiffTree = (data1, data2) => {
   const keys1 = Object.keys(data1)
   const keys2 = Object.keys(data2)
-  const allKeys = [...new Set([...keys1, ...keys2])].sort((a, b) => a.localeCompare(b))
+  const allKeys = _.sortBy([...new Set([...keys1, ...keys2])])
 
   return allKeys.map((key) => {
     const has1 = Object.hasOwn(data1, key)
