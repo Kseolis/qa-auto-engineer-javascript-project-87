f92fa4e6cd17ce55be049e7384b31d463cda6bba
vlad <vladislav.nikolaenko@p2p.org>
2025-10-26 11:19:17 +0700

    refactoring: change the order of checks

--- src/diff/builder.js
+++ src/diff/builder.js
@@ -8,17 +8,17 @@ export const buildDiffTree = (data1, data2) => {
     const has2 = Object.hasOwn(data2, key)
     const [val1, val2] = [data1[key], data2[key]]
 
-    if (has1 && has2 && Object.is(val1, val2)) {
-      return { type: 'unchanged', key, value: val1 }
-    }
-    if (has1 && has2 && !Object.is(val1, val2)) {
-      return { type: 'updated', key, oldValue: val1, newValue: val2 }
-    }
-    if (has1 && !has2) {
-      return { type: 'removed', key, value: val1 }
-    }
     if (!has1 && has2) {
       return { type: 'added', key, value: val2 }
+    }
+    else if (has1 && !has2) {
+      return { type: 'removed', key, value: val1 }
+    }
+    else if (has1 && has2 && Object.is(val1, val2)) {
+      return { type: 'unchanged', key, value: val1 }
+    }
+    else {
+      return { type: 'updated', key, oldValue: val1, newValue: val2 }
     }
   }).filter(Boolean)
 }
